2e129cc388a7fe676bf4e11636c7d9587c8918f5
Victor Castro II <victor.castro ii@mrjaffesclass.com>
2024-04-15 12:49:46 -0700

    jsadkfl

--- MVCExample-master/src/mvc/tictactoe/Model.java
+++ MVCExample-master/src/mvc/tictactoe/Model.java
@@ -25,7 +25,7 @@ private String[][] board;
   public Model(Messenger messages) {
     mvcMessaging = messages;
     
-this.board = new String[3][3];
+
     
 
   }
@@ -40,6 +40,7 @@ this.mvcMessaging.subscribe("newGame", this);
 
   }
   private void newGame() {
+      this.board = new String[3][3];
     for(int row=0; row<this.board.length; row++) {
       for (int col=0; col<this.board[0].length; col++) {
         this.board[row][col] = "";
@@ -47,6 +48,7 @@ this.mvcMessaging.subscribe("newGame", this);
     }
     this.whoseMove = false;
     this.gameOver = false;
+     this.mvcMessaging.notify("gameOver", "");
   }
 
   
@@ -60,28 +62,71 @@ this.mvcMessaging.subscribe("newGame", this);
     
     // playerMove message handler
     if (messageName.equals("playerMove")) {
-      // Get the position string and convert to row and col
-      String position = (String)messagePayload;
-      Integer row = new Integer(position.substring(0,1));
-      Integer col = new Integer(position.substring(1,2));
-      // If square is blank...
-      if (this.board[row][col].equals("")) {
-        // ... then set X or O depending on whose move it is
-        if (this.whoseMove) {
-          this.board[row][col] = "X";
-        } else {
-          this.board[row][col] = "O";
+      if(!gameOver){
+        // Get the position string and convert to row and col
+        String position = (String)messagePayload;
+        Integer row = new Integer(position.substring(0,1));
+        Integer col = new Integer(position.substring(1,2));
+        // If square is blank...
+        if (this.board[row][col].equals("")) {
+          // ... then set X or O depending on whose move it is
+          if (this.whoseMove) {
+            this.board[row][col] = "X";
+            this.whoseMove = !this.whoseMove;
+          } else {
+            this.board[row][col] = "O";
+            this.whoseMove = !this.whoseMove;
+          }
+          // Send the boardChange message along with the new board
+          isWinner();
+          this.mvcMessaging.notify("boardChange", this.board);
         }
-        // Send the boardChange message along with the new board 
-        this.mvcMessaging.notify("boardChange", this.board);
       }
-      
     // newGame message handler
     } else if (messageName.equals("newGame")) {
       // Reset the app state
       this.newGame();
       // Send the boardChange message along with the new board 
       this.mvcMessaging.notify("boardChange", this.board);
+    }
+  }
+  
+   private void isWinner(){
+    for (int i=0; i<3; i++) {
+      if (!board[i][0].isEmpty() && board[i][0].equals(board[i][1]) && board[i][0].equals(board[i][2])){
+        this.mvcMessaging.notify("gameOver", "Winner: " + board[i][0]);
+        this.gameOver = true;
+      }
+      if (!board[0][i].isEmpty() && board[0][i].equals(board[1][i]) && board[0][i].equals(board[2][i]))
+      {
+        this.mvcMessaging.notify("gameOver", "Winner: " +board[0][i]);
+        this.gameOver = true;
+      }
+    }
+
+    // Check the diagonals
+    if (!board[0][0].isEmpty() && board[0][0].equals(board[1][1]) && board[0][0].equals(board[2][2])){
+      this.mvcMessaging.notify("gameOver", "Winner: " +board[0][0]);
+      this.gameOver = true;
+    }
+    if (!board[0][2].isEmpty() && board[0][2].equals(board[1][1]) && board[0][2].equals(board[2][0])){
+      this.mvcMessaging.notify("gameOver", "Winner: " +board[0][2]);
+      this.gameOver = true;
+    }
+
+    //Checks for any legal moves, if none then returns tie
+    boolean tie = true;
+    for(int y = 0; y < 3; y++){
+        for(int x = 0; x < 3; x++){
+            if(board[y][x].isEmpty()){
+                tie = false;
+            }
+        }
+    }
+
+    if(tie){
+        this.gameOver = true;
+        this.mvcMessaging.notify("gameOver", "tie");
     }
   }
 }
--- MVCExample-master/src/mvc/tictactoe/View.java
+++ MVCExample-master/src/mvc/tictactoe/View.java
@@ -219,57 +219,13 @@ boolean gameOver = false;
     boolean whoseTurn = false;
     private void onClick(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_onClick
         JButton button = (JButton)evt.getSource();
-this.mvcMessaging.notify("playerMove", button.getName());
-    if (button.getText().equals("") && !this.gameOver) {
-      String player = (this.whoseTurn) ? "X" : "O";
-      button.setText(player);
-      String winner = this.isWinner();
-      if (!winner.equals("")) {
-          jLabel1.setText(winner+" Wins the game!");
-          this.gameOver = true;
-      }
-      this.whoseTurn = !this.whoseTurn;
-    }
+        this.mvcMessaging.notify("playerMove", button.getName());
     }//GEN-LAST:event_onClick
 
   /**
    * @param args the command line arguments
    */
-private String isWinner() {
-  // Get the text contents of each button.  
-  // Be sure you're accessing the
-  // buttons in the order you want.  
-  // The Netbeans UI sometimes mixes up
-  // the numbers on the button names so they're not in the 
-  // order you expect
-  String[][] status = new String[3][3];
-  status[0][0] = jButton1.getText();
-  status[0][1] = jButton2.getText();
-  status[0][2] = jButton3.getText();
-  status[1][0] = jButton4.getText();
-  status[1][1] = jButton5.getText();
-  status[1][2] = jButton6.getText();
-  status[2][0] = jButton7.getText();
-  status[2][1] = jButton8.getText();
-  status[2][2] = jButton9.getText();
 
-  // Check the rows and columns for a tic tac toe
-  for (int i=0; i<3; i++) {
-    if (!status[i][0].isEmpty() && status[i][0].equals(status[i][1]) && status[i][0].equals(status[i][2]))
-      return status[i][0];
-    if (!status[0][i].isEmpty() && status[0][i].equals(status[1][i]) && status[0][i].equals(status[2][i]))
-      return status[0][i];
-  }
-  
-  // Check the diagonals
-  if (!status[0][0].isEmpty() && status[0][0].equals(status[1][1]) && status[0][0].equals(status[2][2]))
-    return status[0][0];
-  if (!status[0][2].isEmpty() && status[0][2].equals(status[1][1]) && status[0][2].equals(status[2][0]))
-    return status[0][2];
-
-  // If we haven't found it, then return a blank string
-  return "";
-}
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButton1;
     private javax.swing.JButton jButton2;
